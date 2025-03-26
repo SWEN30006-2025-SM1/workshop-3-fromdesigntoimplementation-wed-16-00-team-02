a9688d09d0cebd52b49ca214f90fdc87790a855c
ekansnek <mrekenchen@gmail.com>
2025-03-26 17:52:30 +1100

    implemented person and inheritances

--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -1,5 +1,5 @@
 package com.unimelb.swen30006.workshops;
-import java.time.LocalDate
+import java.time.LocalDate;
 public class Student extends Person {
     private String address;
     private LocalDate dob;
